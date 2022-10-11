f0a120e7f5099e73726aab881fe3264adb7aedfa
Menemi <danila.titov1210@yandex.ru>
2022-10-11 19:35:50 +0300

    feat: add a simple user

---
+++ telegram-queue-bot/TgUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace telegram_queue_bot
+{
+    public class TgUser
+    {
+        public String UserName { get; }
+        public int UserId { get; }
+
+        public TgUser(String Name, int Id)
+        {
+            UserName = Name;
+            UserId = Id;
+        }
+    }
+}
